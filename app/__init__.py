89329472f17e62d0e99f4f1c312c698cce9ac54e
Maciej Krzywicki <maciastydev>
2024-10-02 22:00:26 +0200

    database conf and first call to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 # predeifned variable that set a reference (module name - "app")
+load_dotenv()
 
 app = Flask(__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 # workaround for cyclic import
 from app import routes
 
 
-##
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
-from app import app
+from app import app, db
+from sqlalchemy import text
 # decorator - modifies function that follows it
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello world"
-
-
+@app.route('/users')
+def get_users():
+    result = db.session.execute(text("SELECT * FROM auth.users"))
+    users = []
+    for row in result:
+        users.append(dict(row._mapping))
+    return {"users": users}
 
