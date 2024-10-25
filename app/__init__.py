8176fc7d7b8d5a34f0afaf7f369ad0d1e0ed48d5
Maciej Krzywicki <maciastydev>
2024-10-25 14:10:47 +0200

    Add delete endpoint on links and categories endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -5,37 +5,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 # predeifned variable that set a reference (module name - "app")
-load_dotenv('.flaskenv')
-load_dotenv('.env')
+load_dotenv(".flaskenv")
+load_dotenv(".env")
 
 app = Flask(__name__)
 
 # JWT hashing
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
 # workaround for cyclic import
 
-from app.routes.users import users_bp
-from app.routes.categories import categories_bp
-from app.routes.links import links_bp
+from app.routes.users import users_bp  # noqa: E402
+from app.routes.links import links_bp  # noqa: E402
+from app.routes.categories import categories_bp  # noqa: E402
 
-app.register_blueprint(users_bp, url_prefix='/users')
-app.register_blueprint(categories_bp, url_prefix='/categories')
-app.register_blueprint(links_bp, url_prefix='/links')
+app.register_blueprint(users_bp, url_prefix="/users")
+app.register_blueprint(links_bp, url_prefix="/links")
+app.register_blueprint(categories_bp, url_prefix="/categories")
 
 
 def handle_exception(e):
-    if hasattr(e, 'code'):
-        response = {
-            "message": e.description
-        }
+    if hasattr(e, "code"):
+        response = {"message": e.description}
         return jsonify(response), e.code
     else:
-        response = {
-            "message": str(e)
-        }
+        response = {"message": str(e)}
         return jsonify(response), 500
 
 
--- app/routes/categories.py
+++ app/routes/categories.py
@@ -1,14 +1,99 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required
+from flask import Blueprint, jsonify, request, abort
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import Category, CategorySchema
+from app import db
+from marshmallow import ValidationError
 
-categories_bp = Blueprint('categories', __name__)
+categories_bp = Blueprint("categories", __name__)
 
-@categories_bp.route('', methods=['POST'])
+category_schema = CategorySchema(exclude=["user_id", "id"])
+
+
+@categories_bp.route("", methods=["POST"])
 @jwt_required()
 def add_category():
+    user_id = get_jwt_identity()
+
+    try:
+        category = category_schema.load(request.json)
+        existing_category = Category.query.filter_by(
+            user_id=user_id, name=category["name"]
+        ).first()
+        if existing_category:
+            return abort(
+                400, description="This URL already exists for the user."
+            )
+        category["user_id"] = user_id
+        category = Category(**category)
+        db.session.add(category)
+        db.session.commit()
+        return (
+            jsonify(
+                {
+                    "category_id": category.id,
+                    "message": "Category added!",
+                }
+            ),
+            201,
+        )
+    except ValidationError as e:
+        return abort(400, description=e.messages)
     pass
 
-@categories_bp.route('/<uuid:category_id>', methods=['DELETE'])
+
+@categories_bp.route("/<uuid:category_id>", methods=["PUT"])
+@jwt_required()
+def modify_category(category_id):
+    user_id = get_jwt_identity()
+
+    category = Category.query.get(category_id)
+
+    if not category:
+        return abort(404, description="Category not found")
+    try:
+        new_category_data = category_schema.load(request.json)
+
+        existing_category = Category.query.filter(
+            Category.user_id == user_id,
+            Category.name == new_category_data["name"],
+            Category.id != category_id,
+        ).first()
+        if existing_category:
+            return abort(
+                400, description="This name already exists for the user."
+            )
+
+        for key, value in new_category_data.items():
+            setattr(category, key, value)
+
+        db.session.commit()
+        return (
+            jsonify(
+                {
+                    "category_id": category.id,
+                    "message": "Category Modified!",
+                }
+            ),
+            201,
+        )
+    except ValidationError as e:
+        return abort(400, description=e.messages)
+
+
+@categories_bp.route("/<uuid:category_id>", methods=["DELETE"])
 @jwt_required()
 def delete_category(category_id):
-    pass
+    category = Category.query.get(category_id)
+    if not category:
+        return abort(400, description="Category not found")
+    db.session.delete(category)
+    db.session.commit()
+    return (
+        jsonify(
+            {
+                "category_id": category.id,
+                "message": "Category deleted!",
+            }
+        ),
+        201,
+    )
